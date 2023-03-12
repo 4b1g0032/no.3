32c61389be63735ea960004a0069b4285a55e789
asd05 <asd05@MSI>
2023-03-12 10:51:07 +0800

    加入專案檔案。

---
+++ no.3/no.3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int x, y;
+    while (cin >> x >> y)
+    {
+        if (sqrt(pow(x, 2) + pow(y, 2)) > 200)
+        {
+            cout<<"outside"<<endl;
+        }
+        else
+            cout<<"inside"<<endl;
+    }
+    return 0;
+}
